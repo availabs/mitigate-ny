0bce7b46768dd661b35c0ee2f24553d355e85759
Eric Conklin <eric.s.conklin@gmail.com>
2018-10-03 10:09:58 -0400

    'added_actions_measures_to_hazards'

--- src/pages/auth/RiskIndex/submenus.js
+++ src/pages/auth/RiskIndex/submenus.js
@@ -1,21 +1,22 @@
 export default [
 [
        {name: 'Flooding', path: '/hazards/riverine'},
-       {name: 'Wind', path: '/hazards/wind'},
+       {name: 'Hurricane', path: '/hazards/hurricane'},
        {name: 'Snow Storm', path: '/hazards/winterweat'},
-       {name: 'Tornado', path: '/hazards/tornado'},
        {name: 'Costal Erosion', path: '/hazards/coastal'},
+       {name: 'Wind', path: '/hazards/wind'},
        {name: 'Ice Storm', path: '/hazards/icestorm'},
        {name: 'Hail', path: '/hazards/hail'},
+       {name: 'Tornado', path: '/hazards/tornado'},       
        {name: 'Drought', path: '/hazards/drought'},
        {name: 'Coldwave', path: '/hazards/coldwave'},
        {name: 'Lightning', path: '/hazards/lightning'},
        {name: 'Heat Wave', path: '/hazards/heatwave'},
+       {name: 'Wildfire', path: '/hazards/wildfire'},
        {name: 'Tsunami', path: '/hazards/tsunami'},
        {name: 'Landslide', path: '/hazards/landslide'},
-       {name: 'Wildfire', path: '/hazards/wildfire'},
-       {name: 'Hurricane', path: '/hazards/hurricane'},
-       {name: 'Earthquake', path: '/hazards/earthquake'}
-
+       {name: 'Avalanche', path: '/hazards/avalanche'},
+       {name: 'Earthquake', path: '/hazards/earthquake'},
+       
     ]
 ]
