14278231a42eb5671a08322c3c765d8f9f3e2101
Eric Conklin <eric.s.conklin@gmail.com>
2018-10-15 13:05:08 -0400

    'added-better-footer'

--- src/components/light-admin/containers/Footer.js
+++ src/components/light-admin/containers/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Content from 'components/cms/Content'
 
 export default (props) => (        
 
@@ -6,21 +7,15 @@ export default (props) => (
           <div className='property-info-w'>
           
               <div className="property-info-main" style={{maxWidth: '50%' , borderLeft: 'none'}}>
-                <h3>Contact</h3>
-                  <ul>
-                    <li><a>Eric Krans - ekrans@albany.edu</a></li>
-                    <li><a>Albany Visualization and Informatics Lab</a></li>
-                    <li><a>Room 331</a></li>
-                    <li><a>1215 Western Ave. Albany, NY 12203</a></li>
-                  </ul>
+                <div className="property-section">
+                  <Content content_id={`footer-content`} />
+                </div>
+                
               </div>
             
             <div className="property-info-main" style={{maxWidth: '50%' , borderLeft: 'none'}}>
-              <div>
-                <h3>Connect</h3>
-                  <ul>
-                    <li><a>Submit Questions or Comments</a></li>
-                  </ul>
+              <div className="property-section">
+                  <Content content_id={`footer-comments`} />
                 </div>
               </div>
             </div>
