d781940a17338b5fabc2ef960f7128f23f575e7b
Eric Conklin <eric.s.conklin@gmail.com>
2018-10-25 12:37:35 -0400

    'added-county-hazard-tables'

--- src/pages/auth/Local/local.js
+++ src/pages/auth/Local/local.js
@@ -7,13 +7,14 @@ import CountyPlanChoropleth from "pages/auth/Capabilities/components/CountyPlanC
 import Submenus from './local-submenus'
 import LocalCapabilities from './local-capabilities'
 import HMGPTable from 'pages/auth/RiskIndex/components/HMGPTable'
+import CountyCapabilitiesTable from "components/mitigate-ny/CountyCapabilitiesTable"
 
 class Local extends Component {
   render () {
    return (
       <div className='property-single'>
         <div className='property-info-w'>
-          <div className="property-info-main">
+          <div className="property-info-main" style={{paddingBottom: 0}}>
 
            
            
@@ -24,8 +25,77 @@ class Local extends Component {
 
             <div className="property-section">
               <Content content_id={`capabilities-lhmp`} />
-            </div>  
+            </div>
+            <div className="property-section">
+              <Content content_id={`capabilities-lhmp_hazards`} />
+            </div>
+          </div>
+         </div>
 
+          
+
+            <CountyCapabilitiesTable
+                
+                descriptions={ [
+                     'Avalanche' ,
+                     'Coastal Erosion' ,
+                     'High Wave Action'  ,
+                      'Drought' ,
+                      'Earthquake' ,
+                      'Extreme Cold' ,
+                      'Extreme Heat' ,
+                      'Flood' ,
+                      'Hail' ,
+                      
+                      'High Winds' ,
+                      'Hurricane'  ,
+                      'Ice' ,
+                      'Landslide' ,
+                      'Lightning' ,
+                      'Snow' ,
+                      'Tornado' ,
+                      'Tsunami' ,
+                      'Wildfire' , 
+                      'Volcano' , 
+                                           
+                      ] }/> 
+
+
+        <div className='property-info-w'>
+          <div className="property-info-main" style={{paddingTop: 0, paddingBottom: 0}}>
+            <div className="property-section">
+              <Content content_id={`capabilities-lhmp_hazards-of-concern`} />
+            </div>
+           </div>
+         </div> 
+
+            <CountyCapabilitiesTable
+                
+                descriptions={ [
+                      'Hazardous Materials (HAZMAT fixed and in transit)' ,
+                       'HazMat-Fixed Site' ,
+                       'HazMat-In Transit' ,
+                       'Dam Failure' ,
+                       'Epidemic' ,
+                       'Blight' ,
+                       'Disease Outbreaks' ,
+                       'Infestation / Invasive Species' ,
+                       'Utility Failure' ,
+                       'Severe Storms' ,
+                       'Severe Winter Storms' ,
+                       'Terrorism' ,
+                       'Food Shortage' ,
+                       'Water Supply Contamination' ,
+                       'Air Contamination' , 
+                       'Contamination - air and water' ,
+                       'Transportation Accident'
+                                           
+                      ] }/>          
+
+          
+
+         <div className='property-info-w'>
+          <div className="property-info-main"  style={{paddingTop: 0}}>
             <div className="property-section">
               <Content content_id={`section7-lhmp_submittal_review_process`} />
             </div>
