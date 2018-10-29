6f9c6dbecf65d70907b767ac8c58f42f02bdeea6
Eric Conklin <eric.s.conklin@gmail.com>
2018-10-29 11:03:28 -0400

    'added-methodology-new2019'

--- src/pages/auth/About/about-submenus.js
+++ src/pages/auth/About/about-submenus.js
@@ -3,10 +3,10 @@ export default [
        {name: 'About', path: '/about'},
        {name: 'Mitigation News', path: '/about/news'},
        {name: 'The 2019 SHMP Planning Process', path: '/about/process'},
+       {name: 'New in 2019', path: '/about/new2019'},
        {name: 'Implementation', path: '/about/implementation'},
        {name: 'Methodology', path: '/about/methodology'},
        {name: 'Guiding Authorities', path: '/about/authorities'},
        {name: 'Glossary', path: '/about/glossary'},
-
     ]
 ]
