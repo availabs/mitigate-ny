518ec6826f05bdac00fc7ffe7dc1aa112d3a5a24
Eric Conklin <eric.s.conklin@gmail.com>
2018-11-20 11:41:57 -0500

    'changes-to-the-hazards-submenu'

--- src/pages/auth/RiskIndex/submenus.js
+++ src/pages/auth/RiskIndex/submenus.js
@@ -5,22 +5,28 @@ export default [
        {name: 'Coastal Hazards', path: '/hazards/coastal'},
        {name: 'Coldwave', path: '/hazards/coldwave'},
        {name: 'Drought', path: '/hazards/drought'},
-       {name: 'Earthquake', path: '/hazards/earthquake'},
+      
 ],
 [
+        {name: 'Earthquake', path: '/hazards/earthquake'},
        {name: 'Flooding', path: '/hazards/riverine'},
        {name: 'Hail', path: '/hazards/hail'},
        {name: 'Heat Wave', path: '/hazards/heatwave'},
        {name: 'Hurricane', path: '/hazards/hurricane'},
-       {name: 'Ice Storm', path: '/hazards/icestorm'},
-       {name: 'Landslide', path: '/hazards/landslide'},
+       
 ],
 [
+       {name: 'Ice Storm', path: '/hazards/icestorm'},
+       {name: 'Landslide', path: '/hazards/landslide'},
        {name: 'Lightning', path: '/hazards/lightning'},
        {name: 'Snow Storm', path: '/hazards/winterweat'},
        {name: 'Tsunami', path: '/hazards/tsunami'},
-       {name: 'Tornado', path: '/hazards/tornado'},
+          
+],
+[
+      {name: 'Tornado', path: '/hazards/tornado'},
+       {name: 'Volcano', path: '/hazards/volcano'},
        {name: 'Wildfire', path: '/hazards/wildfire'},
-       {name: 'Wind', path: '/hazards/wind'},   
+       {name: 'Wind', path: '/hazards/wind'},
     ]
 ]
